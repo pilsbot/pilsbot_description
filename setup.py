32dd8f68eed688a57359ac7954d9c24c19c53a05
Pascal Pieper <accounts@pascalpieper.de>
2021-10-12 15:36:19 +0200

    now using command_controller to launch acker_diff_controller

--- setup.py
+++ setup.py
@@ -13,11 +13,12 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*'))
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Johannes Hackbarth',
+    maintainer='Johannes Hackbarth, der Penner',
     maintainer_email='j.hackbarth@cubegaming.de',
     description='TODO: Package description',
     license='TODO: License declaration',
